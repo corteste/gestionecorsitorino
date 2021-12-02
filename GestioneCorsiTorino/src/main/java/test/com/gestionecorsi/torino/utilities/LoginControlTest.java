462309d721fb28e4f582f1aa98546857ffbf4353
Thiefthedark <salvatorepirozzo@outlook.it>
2021-12-02 14:48:15 +0100

    Test Login

---
+++ GestioneCorsiTorino/src/main/java/test/com/gestionecorsi/torino/utilities/LoginControlTest.java
@@ -0,0 +1,14 @@
+package test.com.gestionecorsi.torino.utilities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoginControlTest {
+
+	@Test
+	void testGetAdminPass() {
+		fail("Not yet implemented");
+	}
+
+}
