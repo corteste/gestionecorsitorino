3f1f93fab8457e7ecff05186812a96a1ae97616e
Fabio <fabio.sunna@gmail.com>
2021-12-03 15:42:31 +0100

    updated ddl.sql, insert.sql; created drop.sql

---
+++ GestioneCorsiTorino/SQL/drop.sql
@@ -0,0 +1,8 @@
+drop table corsista cascade constraints;
+drop table corso cascade constraints;
+drop table corsocorsista cascade constraints;
+drop table docente cascade constraints;
+drop sequence corso_seq;
+drop sequence corsista_seq;
+drop view docente_cv;
+drop view corso_popolare;
--- GestioneCorsiTorino/SQL/insert.sql
+++ GestioneCorsiTorino/SQL/insert.sql
@@ -4,7 +4,7 @@ INSERT INTO docente VALUES('00002', 'Jon', 'Snow', 'INFORMATICA;ANALISI');
 INSERT INTO docente VALUES('00003', 'Francesco', 'Totti', 'STATISTICA;FISICA;CHIMICA;DIRITTO');
 
 --admin
-INSERT INTO ADMIN VALUES('Mario','00001','Rossi','prova');
+INSERT INTO ADMIN VALUES('prova','00001','Mario','Rossi');
 
 --corsi
 INSERT INTO CORSO VALUES(corso_seq.nextval,'INFORMATICA','20-SET-2021','20-GEN-2021',1000,'A;B;C;','A1','00001');
@@ -14,8 +14,8 @@ INSERT INTO CORSO VALUES(corso_seq.nextval,'FISICA','20-SET-2021','20-GEN-2021',
 INSERT INTO CORSO VALUES(corso_seq.nextval,'DIRITTO','20-SET-2021','20-GEN-2021',1000,'A;B;C;','A3','00003');
 
 --corsista
+INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'Mickey','Mouse','S');
+INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'Duffy','Duck','N');
+INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'Paperon','De Paperoni','S');
 INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'Pippo','Pluto','S');
-INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'Pipqqq','EERRR','S');
-INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'QWERTY','AMD','S');
-INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'MAX','NUTS','S');
-INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'Sono','un corsista','S');
+INSERT INTO CORSISTA VALUES(corsista_seq.nextval,'Gianni','Neri','S');
