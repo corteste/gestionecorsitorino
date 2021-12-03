8e31e9a9529f5e250d86f419fa0e723fa20e55ec
Thiefthedark <salvatorepirozzo@outlook.it>
2021-12-03 16:49:20 +0100

    provo aggiunta br per andare a capo mess errore

--- GestioneCorsiTorino/src/main/java/com/gestionecorsi/torino/exception/InvalidCorsoException.java
+++ GestioneCorsiTorino/src/main/java/com/gestionecorsi/torino/exception/InvalidCorsoException.java
@@ -14,15 +14,15 @@ public class InvalidCorsoException extends Throwable {
 		// TODO Auto-generated method stub
 		String tmp ="" ;
 		if(!this.b[0])
-			tmp = "Nome corso non valido max 30 caratteri no numeri";
+			tmp =tmp+"Nome corso non valido max 30 caratteri. No numeri <br>";
 		if(!this.b[1])
-			tmp = "Durata corso non valida max 2 GG";
+			tmp =tmp+ "Durata corso non valida max 2 GG <br>";
 		if(!this.b[2])
-			tmp = "Commento troppo lungo max 200 caratteri";
+			tmp =tmp+ "Commento troppo lungo max 200 caratteri <br>";
 		if(!this.b[3])
-			tmp = "Nome aula troppo lungo max 30 caratteri";
+			tmp =tmp+ "Nome aula troppo lungo max 30 caratteri <br>";
 		if(!this.b[4])
-			tmp = "Docente non trovato o non abilitato all'insegnamento del corso scelto";
+			tmp = tmp+"Docente non trovato o non abilitato all'insegnamento del corso scelto <br>";
 		return tmp;
 	}
 	
